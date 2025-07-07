3e106b83cc7e3a9f07bbc2d6ce0e4efa06adbe56
vikraman1653763 <vikraman1653763@gmail.com>
2025-07-07 14:18:29 +0530

    07-07-2025

---
+++ src/component/Cards/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Badges from "../badges/Badges";
+
+const Card = ({ title, description, image, link, techBadges, cta = "Visit Website" }) => {
+  return (
+    <div className="project-card" data-aos="fade-up">
+      <img src={image} alt={title} className="project-image" />
+      <h4 className="project-card-title">{title}</h4>
+      <p className="project-description">{description}</p>
+
+      {techBadges && (
+        <div className="project-badge">
+          <Badges techBadges={techBadges} />
+        </div>
+      )}
+
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="project-link"
+      >
+        {cta}
+      </a>
+    </div>
+  );
+};
+
+export default Card;
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -3,7 +3,7 @@ import "../style/Projects.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Text3 from "./Text3";
-import Badges from "./badges/Badges";
+import Card from "./Cards/Card.jsx";
 
 const Projects = () => {
   useEffect(() => {
@@ -12,8 +12,9 @@ const Projects = () => {
       easing: "ease",
     });
   }, []);
+
   const projects = {
-    work: [
+    main: [
       {
         title: "Xagrotor Tek",
         description:
@@ -41,7 +42,7 @@ const Projects = () => {
       {
         title: "Task Manager Application",
         description:
-          "A full-stack task manager built with MERN with Tailwind CSS includes role-based access, task tracking, report export, and responsive UI .",
+          "A full-stack task manager built with MERN with Tailwind CSS includes role-based access, task tracking, report export, and responsive UI.",
         link: "https://youtu.be/Wk__CVnpkic?si=Vtnr3Q8KcHonab9D",
         image: "/assets/tm.webp",
         techBadges: [
@@ -54,28 +55,46 @@ const Projects = () => {
           "Axios",
           "JWT",
         ],
+        isDemo: true,
       },
-    ],
-    freelance: [
+      {
+        title: "Expense Tracker Application",
+        description:
+          "A full-stack expense tracker built with MERN stack, featuring income & expense management, chart visualization, category-wise insights, and authentication.",
+        link: "https://youtu.be/Qieb2KuvJ44", // update with actual demo link if available
+        image: "/assets/et.webp",
+        techBadges: [
+          "React",
+          "Tailwind",
+          "Node",
+          "Express",
+          "MongoDB",
+          "JWT",
+          "Axios",
+        ],
+        isDemo: true,
+      },
       {
         title: "DragonTech Solutions",
         description:
-          "Designed and developed a robust IT solutions platform, handling both frontend and backend as part of a 2-member team",
+          "Designed and developed a robust IT solutions platform, handling both frontend and backend as part of a 2-member team.",
         link: "https://dragontechsolutions.com",
         image: "/assets/dt.webp",
-         techBadges: [
-         "React" , "Node" , "Express" ,'Cpanel' ,'SMTP'
-        ],
+        techBadges: ["React", "Node", "Express", "Cpanel", "SMTP"],
       },
       {
         title: "Venkataeswara Hospitals",
         description:
-          "Developed a modern and responsive website to enhance patient engagement and streamline hospital services",
-
+          "Developed a modern and responsive website to enhance patient engagement and streamline hospital services.",
         link: "https://www.vhospitals.com",
         image: "/assets/vh.webp",
-         techBadges: [
-         "React" , "Node" ,"Framer Motion", "Express" ,'Cpanel' ,'SMTP'
+        techBadges: [
+          "React",
+          "Node",
+          "Framer Motion",
+          "Express",
+          "Cpanel",
+          "SMTP",
         ],
       },
     ],
@@ -86,9 +105,7 @@ const Projects = () => {
           "Developed a dashboard for users with dynamic mapping, workflow management, and secure login using OTP verification. Integrated geospatial data via GeoServer and PostgreSQL.",
         link: "https://www.linkedin.com/in/vikraman-g/overlay/1714546205584/single-media-viewer/?profileId=ACoAAEAwQyMBGlmKid_VvSAIQYSCrS6eBSdYaXY",
         image: "/assets/webgis.webp",
-         techBadges: [
-          "Python","Flask","MapBox","PostgreSql","Geoserver"
-        ],
+        techBadges: ["Python", "Flask", "MapBox", "PostgreSql", "Geoserver"],
       },
       {
         title: "Full Stack Developer Intern",
@@ -96,9 +113,7 @@ const Projects = () => {
           "Created a fault detection website using YOLOv8 for defect detection, a user-friendly UI for image editing, and MySQL for data storage.",
         link: "https://www.linkedin.com/in/vikraman-g/overlay/1708329257687/single-media-viewer/?profileId=ACoAAEAwQyMBGlmKid_VvSAIQYSCrS6eBSdYaXY",
         image: "/assets/aitool.webp",
-         techBadges: [
-          "Jinja","Flask" ,"Python" ,"MySql"
-        ],
+        techBadges: ["Jinja", "Flask", "Python", "MySql"],
       },
       {
         title: "Web Developer Intern",
@@ -106,9 +121,7 @@ const Projects = () => {
           "Built an Ecommerce website focusing on user experience and feature-rich functionalities, showcasing my full-stack skills.",
         link: "https://www.linkedin.com/in/vikraman-g/overlay/1708328853862/single-media-viewer/?profileId=ACoAAEAwQyMBGlmKid_VvSAIQYSCrS6eBSdYaXY",
         image: "/assets/watch.webp",
-         techBadges: [
-         "React" ,"Bootstrap","CSS","Git" ,"Netlify"
-        ],
+        techBadges: ["React", "Bootstrap", "CSS", "Git", "Netlify"],
       },
     ],
   };
@@ -116,113 +129,40 @@ const Projects = () => {
   return (
     <section className="projects-container" id="work">
       <Text3 className="code-icon" />
+      <h2 className="projects-title">What I’ve Built</h2>
 
-      <h2 className="projects-title">Projects</h2>
-      <section className="projects-work-free">
-        {/* Work Section */}
-        <div className="projects-section">
-          <h3 className="projects-subtitle">Work</h3>
-          <div className="projects-cards work" data-aos="fade-up">
-            {projects.work.map((project, index) => (
-              <div key={index} className="project-card">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="project-image"
-                />
-                <h4 className="project-card-title">{project.title}</h4>
-                <p className="project-description">{project.description}</p>
-                
-                {project.techBadges && (
-                  <div className="project-badge">
-                    <Badges techBadges={project.techBadges} />
-                  </div>
-                )}
-
-
-                {project.title === "Task Manager Application" ? (
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="project-link"
-                  >
-                    View Demo
-                  </a>
-                ) : (
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="project-link"
-                  >
-                    Visit Website
-                  </a>
-                )}
-              </div>
-            ))}
-          </div>
+      {/* Combined Projects Section */}
+      <div className="projects-section">
+        <h3 className="projects-subtitle">Projects</h3>
+        <div className="projects-cards main">
+          {projects.main.map((project, index) => (
+            <Card
+              key={index}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              link={project.link}
+              techBadges={project.techBadges}
+              cta={project.isDemo ? "View Demo" : "Visit Website"}
+            />
+          ))}
         </div>
-
-        {/* Freelance Section */}
-        <div className="projects-section">
-          <h3 className="projects-subtitle">Freelance</h3>
-          <div className="projects-cards freelance" data-aos="fade-up">
-            {projects.freelance.map((project, index) => (
-              <div key={index} className="project-card">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="project-image"
-                />
-                <h4 className="project-card-title">{project.title}</h4>
-                <p className="project-description">{project.description}</p>
-                {project.techBadges && (
-                  <div className="project-badge">
-                    <Badges techBadges={project.techBadges} />
-                  </div>
-                )}
-                <a
-                  href={project.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="project-link"
-                >
-                  Visit Website
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
+      </div>
 
       {/* Internship Section */}
       <div className="projects-section">
         <h3 className="projects-subtitle">Internships</h3>
         <div className="projects-cards internship">
           {projects.internship.map((project, index) => (
-            <div key={index} className="project-card" data-aos="fade-up">
-              <img
-                src={project.image}
-                alt={project.title}
-                className="project-image"
-              />
-              <h4 className="project-card-title">{project.title}</h4>
-              <p className="project-description">{project.description}</p>
-               {project.techBadges && (
-                  <div className="project-badge">
-                    <Badges techBadges={project.techBadges} />
-                  </div>
-                )}
-              <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                Learn More
-              </a>
-            </div>
+            <Card
+              key={index}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              link={project.link}
+              techBadges={project.techBadges}
+              cta="Learn More"
+            />
           ))}
         </div>
       </div>
